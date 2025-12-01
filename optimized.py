8aafedc5bbba82f5cf17e8d60e0aa82e3ee9302a
meridien22 <meridien.sig@gmail.com>
2025-12-01 08:14:05 +0100

    Optimized

--- optimized.py
+++ optimized.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import time
+import pandas
 from rich.console import Console
 from rich.panel import Panel
 from rich.align import Align
@@ -19,10 +20,9 @@ def data_loading(path):
     Returns:
         dict: dictionnaire contenant les actions, leurs coûts et leurs bénéfices
     """
-    with open(path, mode='r', encoding='utf-8') as csv_file:
-        reader = csv.DictReader(csv_file)
-        dict_ = list(reader)
-    return dict_
+    data_frame = pandas.read_csv(path)
+    return data_frame.to_dict(orient='records')
+
 
 def data_preparation(dict_):
     """Prépare le jeu de donnée pour l'analyse.
@@ -56,33 +56,44 @@ def data_transformation(dict_):
 
     return list_tuple
 
-def calculate_max_profit(limit, list_, action_selection = []):
+def calculate_max_profit(limit, list_):
     """Détermine la combinsaison d'action qui générera le profit le plus élevé.
 
     Args:
         limit (intger): limite des dépenses
         list_ (list): Liste contenant les actions
-        action_selection (list, optional): Les actions à acheter pour atteindre le bénéfice maximum.
 
     Returns:
         tuple : le profit maximum, la liste des actions à acheter
     """
-    if list_:
-        # cas ou on n'achète pas l'action, on appelle récusrsivement la fonction
-        # en enlevant un élément de la liste et sans modifier la limite
-        benefit1, action_selection_1 = calculate_max_profit(limit, list_[1:], action_selection)
-        action = list_[0]
-        if action[1] <= limit:
-            # cas ou on peut on et achète l'action, on appelle récusrsivement la fonction
-            # en enlevant un élément de la liste, en modifier la limite et en ajoutant l'action
-            # a la liste des actions selectionnées
-            benefit2, action_selection_2 = calculate_max_profit(limit - action[1], list_[1:], action_selection + [action])
-            if benefit1 < benefit2:
-                return benefit2, action_selection_2
+    # On crée un tableau en 2 dimensions rempli de 0 avec (limit + 1) colonnes et (nombre d'action + 1) lignes
+    # On ajoute 1 au nombre d'action pour gérer le cas ou l'on souhaite calculer le bénéfice maximum sans aucune action
+    # On ajoute 1 a la limit pour gérer le cas ou cette limite serait égal à zéro
+    matrice = [[0 for x in range(limit + 1)] for x in range(len(list_) + 1)]
 
-        return benefit1, action_selection_1
-    else:
-        return sum([action[2] for action in action_selection]), action_selection
+    for act in range(1, len(list_) + 1):
+        for lim in range(1, limit + 1):
+            # Le coût de l'action est <= à la limite, on peut acheter l'action
+            if list_[act-1][1] <= lim:
+                # On prend la valeur maximale entre le bénéfice calculé précédement pour la limite 
+                # et le benefice de l'action + le bénéfice calculé précédement pour la limite - le coût de cette action
+                matrice[act][lim] = max(list_[act-1][2] + matrice[act-1][lim-list_[act-1][1]], matrice[act-1][lim])
+            # On ne peut pas acheter l'action, la matrice prend la même valeur que l'action précédente pour la même limite
+            else:
+                matrice[act][lim] = matrice[act-1][lim]
+
+    # On retrouve maintenant les actions sélectionnées en parcourant la matrice de la fin vers le début
+    lim = limit
+    act = len(list_)
+    action_selection = []
+    while lim >= 0 and act >= 0:
+        action = list_[act-1]
+        if matrice[act][lim] == matrice[act-1][lim-action[1]] + action[2]:
+            action_selection.append(action)
+            lim -= action[1]
+        act -= 1
+
+    return matrice[-1][-1], action_selection
     
 def display_result(result, data, time_execute):
     console = Console()
@@ -108,7 +119,7 @@ def display_result(result, data, time_execute):
     table.add_row("Capital dépensé", f"{str(spent)} €")
     table.add_row("Nombre d'action à acheter", str(len(result[1])))
     table.add_row("Nombre d'action analysée", str(len(data)))
-    table.add_row("Nombre de combinaison analysée", str(2**len(data)))
+    table.add_row("Nombre de combinaison analysée", str(500*len(data)))
     table.add_row("Temps de calcul", f"{str(time_execute)} seconde(s)")
     console.print(table)
 
@@ -117,7 +128,7 @@ def display_result(result, data, time_execute):
     table.add_column("Coût", justify = "right", style = "blue", width=26)
     table.add_column("Bénéfice", justify = "right", style = "blue", width=25)
     for action in action_list:
-        table.add_row(action[0], str(action[1]), f"{str(round(action[2],2))} €")
+        table.add_row(action[0], str(action[1]), f"{str(round(action[2],2))} %")
     console.print(table)
 
 
